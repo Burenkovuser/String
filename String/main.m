7be2c642082cf4724532cdbc6fa951a2b66b22f5
Burenkovuser <nb2207@yandex.ru>
2017-05-11 17:26:58 +0600

    Kochan

--- String/main.m
+++ String/main.m
@@ -10,6 +10,8 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        
+        /*
         //создание строки
         NSString *string = @"sting";
         NSLog(@"%@",string);
@@ -72,6 +74,49 @@ int main(int argc, const char * argv[]) {
         result1 = [string10 compare:string5 options:NSNumericSearch];
         //result1 = 1 (NSOrderedDescending)
         
+        */
+        
+        //Кочан
+        NSString *str1 = @"Это строка А";
+        NSString *str2 = @"Это строка В";
+        NSString *res;
+        NSComparisonResult compareResult;//будет содержать результат операции сравнения строк
+        
+        //Подсчет числа знаков
+        NSLog(@"Длина str1: %lu", [str1 length]);
+        
+        //Копирование одной строки в другую
+        res = [NSString stringWithString:str1];
+        NSLog(@"Копия: %@", res);
+        
+        //Копирование одной строки в конец другой
+        str2 = [str1 stringByAppendingString:str2];
+        NSLog(@"Объединение %@", str2);
+        
+        //Проверка двух строк на равенство
+        if ([str1 isEqualToString:res]) {
+            NSLog(@"str1 == res");
+        } else {
+            NSLog(@"str1 != res");
+        }
+        
+        //Проверка одна строка меньше, равна или больше чем другая
+        compareResult = [str1 compare:str2];
+        if (compareResult == NSOrderedAscending) //упорядочить по взрастанию
+            NSLog(@"str1 < str2");
+        else if (compareResult == NSOrderedSame)
+            NSLog(@"str1 == str2");
+        else if (compareResult == NSOrderedDescending)
+            NSLog(@"str1 > str2");
+        
+        //Преобразование букв в ВВЕРХНИЙ РЕГИСТР
+        res = [str1 uppercaseString];
+        NSLog(@"Преобразование в верхний регистр %s", [res UTF8String]);
+        
+        //Преобразование в нижний регистр
+        res = [str1 lowercaseString];
+        NSLog(@"Преобразование в нижний регистр: %@", res);
+        NSLog(@"Оригинальная строка %@", str1);
     }
     return 0;
 }
